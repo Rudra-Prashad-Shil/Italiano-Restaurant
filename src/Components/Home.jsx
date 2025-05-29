fbe54c57b0e5572217c11bb39de1b58f87b897dc
Rudra-Prashad-Shil <rudrashil016@gmail.com>
2025-05-30 01:49:21 +0600

    Navbar reworked & Banner Completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
                         src={Banner}
                         alt="Shoes" />
                 </figure>
-                <div className="card-body">
+                <div className="card-body mt-5 md:mt-15 lg:mx-auto">
                     <h2 className="card-title text-5xl font-semibold">We Deliver the Best Taste of Food</h2>
-                    <span className="text-xl">Experience our expertly crafted dishes, made from the freshest ingredients.</span>
-                    <div className="">
+                    <span className="text-xl mt-3">Experience our expertly crafted dishes, made from the freshest ingredients.</span>
+                    <div className="relative mt-7 md:mt-15 lg:ml-24">
                         <form action="">
-                            <input className="bg-white p-5 rounded-4xl text-black w-full" type="text" name="" id="" />
-                            <button className="btn bg-orange-600 rounded-4xl p-5 text-white text-xl font-extralight md:absolute md:right-[3vw] md:top-31">search</button>
+                            <input className="bg-white p-5 rounded-4xl text-black w-full md:max-w-lg" type="text" name="" id="" />
+                            <button className="btn bg-orange-600 rounded-4xl sm:px-10 py-6  text-white sm:text-xl font-extralight absolute right-3 top-1.5 md:right-auto md:left-[370px]">Search</button>
                         </form>
                     </div>
                 </div>
