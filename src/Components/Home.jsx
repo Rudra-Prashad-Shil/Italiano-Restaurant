9008a7f8bdbf615b6fd10b08f1d4cb287d91eff7
Rudra-Prashad-Shil <rudrashil016@gmail.com>
2025-06-01 01:05:36 +0600

    Navbar Touch, banner Rework and Floating cards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,13 +4,14 @@ const Home = () => {
     return (
         <div>
             {/* Banner */}
-            <div className="card bg-base-100 image-full w-full shadow-sm brightness-200">
+            <div className="card image-full min-h-[550px] max-h-[850px] shadow-sm brightness-200 relative">
                 <figure>
                     <img
                         src={Banner}
+                        className="w-full "
                         alt="Shoes" />
                 </figure>
-                <div className="card-body mt-5 md:mt-15 lg:mx-auto">
+                <div className="card-body mb-10 mt-5 md:mt-15 lg:mt-30 lg:mx-auto">
                     <h2 className="card-title text-5xl font-semibold">We Deliver the Best Taste of Food</h2>
                     <span className="text-xl mt-3">Experience our expertly crafted dishes, made from the freshest ingredients.</span>
                     <div className="relative mt-7 md:mt-15 lg:ml-24">
@@ -21,8 +22,32 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-
-            
+            {/* Floating Cards */}
+            <div className="relative -mt-10 flex justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4">
+                    <div className="card max-w-96 bg-gray-800 card-md shadow-sm mx-auto">
+                        <div className="card-body p-10">
+                            <h2 className="text-amber-500 text-xl">Step 1</h2>
+                            <h2 className="card-title text-2xl font-bold text-white">Choose Location</h2>
+                            <p className="text-gray-400">Enter your address or choose your current location using app</p>
+                        </div>
+                    </div>
+                    <div className="card max-w-96 bg-gray-800 card-md shadow-sm mx-auto">
+                        <div className="card-body p-10">
+                            <h2 className="text-amber-500 text-xl">Step 2</h2>
+                            <h2 className="card-title text-2xl font-bold text-white">Order Favourite Food</h2>
+                            <p className="text-gray-400">Choose your Favourite Food & your preferred payment method</p>
+                        </div>
+                    </div>
+                    <div className="card max-w-96 bg-gray-800 card-md shadow-sm mx-auto">
+                        <div className="card-body p-10">
+                            <h2 className="text-amber-500 text-xl">Step 3</h2>
+                            <h2 className="card-title text-2xl font-bold text-white">Fast Delivery</h2>
+                            <p className="text-gray-400">Get it delivered at your doorstep within an hour or less</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
